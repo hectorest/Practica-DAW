908d933a542a19e08593b329ee6c0cc28cb587e7
hectorest <33354984+hectorest@users.noreply.github.com>
2018-10-25 15:05:11 +0200

    Revert "modales en Registro y Login"
    This reverts commit 00508acd1a0fa34168162882ab7d07bd054767c2.

--- formulario_acceso.php
+++ formulario_acceso.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 	require_once("head.php");
 	require_once("header.php");
@@ -35,4 +36,43 @@
 
 <?php
 	require_once("footer.php");
+=======
+<?php
+	require_once("head.php");
+	require_once("header.php");
+?>
+
+	<?php
+		if(isset($_GET["er"])){
+			echo "<p>Usuario o contraseña incorrectos</p>";
+		}
+	?>
+		<form action="controlLogin.php" method="post" class="formulario" id="formAcc">
+
+		<fieldset>
+
+			<legend>
+				Iniciar Sesión
+			</legend>
+
+			<p>
+				<label for="usuario">Usuario:</label>
+				<input type="text" minlength="6" maxlength="14" required name="login" id="usuario"/>
+			</p>
+			<p>
+				<label for="passw">Contraseña:</label>
+				<input type="password" minlength="8" maxlength="16" required name="pass" id="passw"/>
+			</p>
+			<p>
+				<button type="submit">Entrar</button> <!--En realidad, el boton submit, una vez pulsado, redirigira, si todos los datos son correctos, a la pagina de respuesta de la solicitud de album-->
+			</p>
+			<a href="formulario_registro.php"><span class="icon-user-plus">¿Aún no tienes una cuenta? Regístrate</span></a>
+
+		</fieldset> 
+
+		</form>
+
+<?php
+	require_once("footer.php");
+>>>>>>> parent of 00508ac... modales en Registro y Login
 ?>
--- formulario_registro.php
+++ formulario_registro.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 	require_once("head.php");
 	require_once("header.php");
@@ -104,4 +105,112 @@
 		</form> 
 <?php
 	require_once("footer.php");
+=======
+<?php
+	require_once("head.php");
+	require_once("header.php");
+?>
+	
+		<form action="controlRegistro.php" method="post" class="formulario" id="formReg">
+			
+			<fieldset>
+
+					<legend>
+						Regístrate
+					</legend>
+				<p>
+					<label for="usuario">Usuario*:</label>
+					<input type="text" pattern="[A-Za-z0-9]{6,14}" minlength="6" maxlength="14" title="El nombre de usuario tendrá como mínimo 6 caracteres que pueden ser tanto letras mayúsculas como minúsculas y como números. El máximo número de caracteres permitido es 14. Los espacios en blanco no están permitidos" required name="usuario" id="usuario"/>
+				</p>
+				<p>
+					<label for="passw1">Contraseña*:</label>
+					<input type="password" pattern="[A-Za-z0-9¿?¡!-_@#$%&=]{8,16}" minlength="8" maxlength="16" required name="passw1" id="passw1" title="La contraseña tendrá un mínimo de 8 caracteres y un máximo de 16. Podrás escribir tanto letras mayúsculas como minúsculas además de números y los siguientes símbolos: ¿?¡!-_@#$%&=. Los espacios en blanco no están permitidos" />
+				</p>
+				<p>
+					<label for="passw2">Repetir contraseña*:</label>
+					<input type="password" pattern="[A-Za-z0-9¿?¡!-_@#$%&=]{8,16}" minlength="8" maxlength="16" required name="passw2" id="passw2" title="La contraseñ;a debe de coincidir con la escrita en la casilla anterior" />
+					<?php
+
+						if(!empty($_GET["er"])){
+							echo "<span>Las contraseñas no coinciden</span>";
+						}
+
+					?>
+				</p>
+				<p>
+					<label for="email">Email*:</label>
+					<input type="email" required name="email" id="email"/>
+				</p>
+				<p>
+					<label for="sexo">Sexo*:</label>
+					<select id="sexo" name="sexo" required>
+						<option value="">Escoge</option>
+						<option>Hombre</option>
+						<option>Mujer</option>
+						<option>Otro</option>
+					</select>
+				</p>
+				<p>
+					<label for="fnac">Fecha de nacimiento*:</label>
+					<input type="date" required name="fnac" id="fnac"/>
+				</p>
+				<p>
+					<label for="cres">Ciudad de residencia*:</label>
+					<select required name="cres" id="cres">
+						<option value="">Escoge</option>
+						<option>San Vicente</option>
+						<option>Elche/Elx</option>
+						<option>Elda</option>
+						<option>Alicante</option>
+						<option>Castellón</option>
+						<option>Valencia</option>
+						<option>Sevilla</option>
+						<option>Murcia</option>
+						<option>Madrid</option>
+						<option>Pekín</option>
+						<option>Tokio</option>
+					</select>
+				</p>
+				<p>
+					<label for="pres">País de residencia*:</label>
+					<select required name="pres" id="pres">
+						<option value="">Escoge</option>
+						<optgroup label="Europa">
+							<option>Alemania</option>
+							<option>España</option>
+							<option>Francia</option>
+							<option>Inglaterra</option>
+							<option>Rusia</option>
+							<option>Suiza</option>
+						</optgroup>
+						<optgroup label="Asia">
+							<option>China</option>
+							<option>Japón</option>
+						</optgroup>
+						<optgroup label="Norteamérica">
+							<option>Estados Unidos</option>
+							<option>Canadá</option>
+						</optgroup>
+						<optgroup label="Centroamérica">
+							<option>México</option>
+						</optgroup>
+						<optgroup label="Sudamérica">
+							<option>Argentina</option>
+							<option>Brasil</option>
+						</optgroup>
+					</select>
+				</p>
+				<p>
+					<label for="fper" id="labfper">Foto de perfil*:</label>
+					<input type="file" accept="image/*" required name="fper" id="fper"/>
+				</p>
+				<p>
+					<button type="submit">Enviar</button> <!--En realidad, el boton submit, una vez pulsado, redirigira, si todos los datos son correctos, a la pagina de respuesta de la solicitud de album-->
+				</p>
+				<a href="formulario_acceso.php"><span class="icon-user">¿Ya tienes una cuenta? Inicia sesión</span></a>
+			</fieldset>
+		</form> 
+<?php
+	require_once("footer.php");
+>>>>>>> parent of 00508ac... modales en Registro y Login
 ?>
