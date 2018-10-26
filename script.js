8a6ee5ec518f1284fc44372ee9118dead2b89c97
hectorest <33354984+hectorest@users.noreply.github.com>
2018-10-26 13:51:44 +0200

    Actualizacion nueva

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function cerrarMensajeModal(){
 	var button = document.querySelector("body nav + button");
 	modal.remove();
 	button.remove();
+
+	/*Hago una recarga de pagina para eliminar el parametro de la url*/
+	
+	var url = location.href;
+	var arrayUrl = url.split("?");
+	url = arrayUrl[0];
+	url.toString();
+	url = url + "#";
+	location.href = url;
 }
