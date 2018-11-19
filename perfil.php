87f32f624d4c8a84cd05040d340afd0305dd4c17
hectorest <33354984+hectorest@users.noreply.github.com>
2018-11-19 19:51:26 +0100

    1

--- perfil.php
+++ perfil.php
@@ -42,7 +42,7 @@ modalPerfil;
 		echo $idUsuSesion;
 		 
 		 // Ejecuta una sentencia SQL 
-		 $sentencia = 'SELECT * FROM usuarios u, paises p where p.IdPais=u.Pais and u.IdUsuario='.$idUsuSesion; 
+		  $sentencia = 'SELECT * FROM usuarios u, paises p where p.IdPais=u.Pais and u.IdUsuario='.$idUsuSesion; 
 		 if(!($resultado = $GLOBALS["mysqli"]->query($sentencia))) { 
 		   echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $GLOBALS["mysqli"]->error; 
 		   echo '</p>'; 
