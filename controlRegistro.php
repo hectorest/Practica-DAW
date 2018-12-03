75a9c1b452385dbc3f691e63dcc76f1e64bfaaf1
JaviGF98 <43470986+JaviGF98@users.noreply.github.com>
2018-12-03 03:33:54 +0100

    Actualización Nueva
    Ya he hecho mi parte, a ti te toca revisar por si me falta mostrar algun mensaje de error o algo o si algo no te cuadra, además de hacer las siguientes páginas: página de respuesta de solicitud de album, página de borrar datos del usuario y página de añadir foto a álbum.
    
    Ignora lo que te comenté en la anterior actualización, al final lo he hecho así.
    
    He actualizado también la base de datos, en caso de que me encuentres algún error o lo que sea trata de corregírmelo, yo lo he mirado de la manera más concienzuda que he podido.
    
    Ya cuando puedas, lo ves todo y me comentas que te parece (por cierto, es todo bastante complejo, al menos a mi parecer XD).

--- controlRegistro.php
+++ controlRegistro.php
@@ -18,26 +18,17 @@ if($sanearPost["Clave"] != $sanearPost["passw2"]){
 	$extra = 'formulario_registro.php';
 	header("Location: http://$host$uri/$extra?er=300");
 }else{
-	if(!isset($_SESSION["usuarioLog"])){
-		echo<<<modalControlRegistro
-
-		<button type="button" onclick="cerrarMensajeModal(2);">X</button>
-		<div class="modal">
-			<div class="contenido">
-			<span>
-				<img src="./img/error.png" alt="error-control-registro">
-				<h2>Error</h2>
-			</span>
-				<p>No has enviado los datos para registrarte</p>
-				<button type="button" onclick="cerrarMensajeModal(2);">Cerrar</button>
-			</div>
-		</div>
-
-modalControlRegistro;
+	if(!isset($_SERVER["HTTP_REFERER"])){
+		$serverCorrecto = false;
+		$host = $_SERVER['HTTP_HOST']; 
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
+		$extra = 'formulario_registro.php';
+		header("Location: http://$host$uri/$extra?er=310");
 		exit;
 	}
 	else{
-		if(!isset($_SERVER["HTTP_REFERER"])){
+		$url = parse_url($_SERVER["HTTP_REFERER"]);
+		if($url["host"] != $_SERVER["SERVER_NAME"]){
 			$serverCorrecto = false;
 			$host = $_SERVER['HTTP_HOST']; 
 			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
@@ -46,83 +37,81 @@ modalControlRegistro;
 			exit;
 		}
 		else{
-			$url = parse_url($_SERVER["HTTP_REFERER"]);
-			if($url["host"] != $_SERVER["SERVER_NAME"]){
-				$serverCorrecto = false;
-				$host = $_SERVER['HTTP_HOST']; 
-				$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
-				$extra = 'formulario_registro.php';
-				header("Location: http://$host$uri/$extra?er=310");
-				exit;
-			}
-			else{
-				$serverCorrecto = true;
-			}
+			$serverCorrecto = true;
 		}
 	}
+}
 	
 
 	//insertar datos base de datos
 	require_once("validarRegistro.php");
 
-	if($serverCorrecto && $datosCorrectos){
+	if($serverCorrecto){
 
-		//primero comprobamos si existe ya el nombre de usuario
-		$sentencia = 'SELECT * FROM usuarios WHERE NomUsuario =' . "'" . $sanearPost["NomUsuario"] . "'";
-		if(!($resultado = $mysqli->query($sentencia))) { 
-			echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
-			echo '</p>'; 
-			exit; 
-		}
-		if(mysqli_num_rows($resultado)){
-			$host = $_SERVER['HTTP_HOST']; 
-			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
-			$extra = 'formulario_registro.php';
-			header("Location: http://$host$uri/$extra?er=301");
-		}
-		else{
+		if($datosCorrectos){
 
-			require_once("rellenarInsertarDatosRegistro.php");
-			$insertarDatos = $insertarDatos . "SYSDATE(),1";
-
-			$sentencia = 'INSERT INTO usuarios (IdUsuario, NomUsuario, Clave, Email, Sexo, FNacimiento, Ciudad, Pais, Foto, FRegistro, Estilo) VALUES (' . $insertarDatos . ')';
+			//primero comprobamos si existe ya el nombre de usuario
+			$sentencia = 'SELECT * FROM usuarios WHERE NomUsuario =' . "'" . $sanearPost["NomUsuario"] . "'";
 			if(!($resultado = $mysqli->query($sentencia))) { 
 				echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
 				echo '</p>'; 
 				exit; 
 			}
-			if($mysqli->affected_rows >= 1){
-				
-				//pasamos a mostrar la tabla con los datos del usuario registrado
+			if(mysqli_num_rows($resultado)){
+				$host = $_SERVER['HTTP_HOST']; 
+				$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
+				$extra = 'formulario_registro.php';
+				header("Location: http://$host$uri/$extra?er=301");
+			}
+			else{
 
-				require_once("head.php");
-				require_once("header.php");
-				require_once("barraNavSesionNoIniciada.php");
+				require_once("rellenarInsertarDatosRegistro.php");
+				$insertarDatos = $insertarDatos . "SYSDATE(),1";
 
-				require_once("escribirTablaRegNuevoUsuario.php");
+				$sentencia = 'INSERT INTO usuarios (IdUsuario, NomUsuario, Clave, Email, Sexo, FNacimiento, Ciudad, Pais, Foto, FRegistro, Estilo) VALUES (' . $insertarDatos . ')';
+				if(!($resultado = $mysqli->query($sentencia))) { 
+					echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
+					echo '</p>'; 
+					exit; 
+				}
+				if($mysqli->affected_rows >= 1){
+					
+					//pasamos a mostrar la tabla con los datos del usuario registrado
 
+					require_once("head.php");
+					require_once("header.php");
+					require_once("barraNavSesionNoIniciada.php");
+
+					require_once("escribirTablaRegNuevoUsuario.php");
+
+				}
 			}
 		}
-	}
-	else{
+		else{
 
-		echo<<<modalControlRegistro
+			echo<<<modalControlRegistro
 
-			<button type="button" onclick="cerrarMensajeModal(2);">X</button>
-			<div class="modal">
-				<div class="contenido">
-				<span>
-					<img src="./img/error.png" alt="error-control-registro">
-					<h2>Error</h2>
-				</span>
-					<p>Los datos enviados se han corrompido. Anulado el registro del nuevo usuario</p>
-					<button type="button" onclick="cerrarMensajeModal(2);">Cerrar</button>
+				<button type="button" onclick="cerrarMensajeModal(2);">X</button>
+				<div class="modal">
+					<div class="contenido">
+					<span>
+						<img src="./img/error.png" alt="error-control-registro">
+						<h2>Error</h2>
+					</span>
+						<p>Los datos enviados se han corrompido. Anulado el registro del nuevo usuario</p>
+						<button type="button" onclick="cerrarMensajeModal(2);">Cerrar</button>
+					</div>
 				</div>
-			</div>
 
 modalControlRegistro;
 
 		}
+	}
+	else{
+		$host = $_SERVER['HTTP_HOST']; 
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');  
+		$extra = 'formulario_registro.php';
+		header("Location: http://$host$uri/$extra?er=310");
 	}
 }
 else{
