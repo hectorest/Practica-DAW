50195e85f5d9b1a791d13af5f8e5ec5e8d56f983
hectorest <33354984+hectorest@users.noreply.github.com>
2018-12-06 21:01:22 +0100

    definitiva

--- validarSolicitud.php
+++ validarSolicitud.php
@@ -127,7 +127,7 @@ $continuo = true;
 		}
 		if($key == "frecep"){
 			if(!empty($value)){
-				$sentenciaPost = $sentenciaPost." and Fecha = $value ";
+				$sentenciaPost = $sentenciaPost." and Fecha = '$value' ";
 			}
 			else{
 				$sentenciaPost = $sentenciaPost." and Fecha = '' ";
@@ -158,6 +158,7 @@ $continuo = true;
 	}
 
 		$sentencia = 'SELECT*FROM solicitudes WHERE '.$sentenciaPost;
+		echo "$sentencia";
 		if(!($resultado = $mysqli->query($sentencia))) { 
 			echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $mysqli->error; 
 			echo '</p>'; 
@@ -172,7 +173,6 @@ $continuo = true;
 
 		foreach ($sanearPost as $key => $value) {
 			if($key == "album"){
-					echo "valido 1";
 				if(!empty($value)){
 					$sentencia = 'SELECT IdAlbum FROM albumes WHERE IdAlbum = ' . $value;
 					if(!($resultado = $GLOBALS["mysqli"]->query($sentencia))) { 
@@ -185,43 +185,36 @@ $continuo = true;
 						$datosCorrectos = true;
 					}else{
 						$datosCorrectos = false;
-						echo "no valido 1";
 						break;
 					}
 					
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 1";
 					break;
 				}
 			}					
 			if($key == "email"){
-				echo "valido 2";
 				if(!empty($value)){
 					if(preg_match_all($expReg[2], $value)){
 						$datosCorrectos = true;
 					}
 					else{
 						$datosCorrectos = false;
-						echo "no valido 2";
 						break;
 					}
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 2";
 					break;
 				}
 			}
 			if($key == "pais"){
-				echo "valido 3";
 				if(!empty($value)){
 
 					$sentencia = 'SELECT IdPais FROM paises WHERE IdPais = ' . $value;
 					if(!($resultado = $GLOBALS["mysqli"]->query($sentencia))) { 
-						echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $GLOBALS["mysqli"]->error; 
-						echo '</p>'; 
+						echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . $GLOBALS["mysqli"]->error;  
 						exit; 
 					}
 
@@ -229,17 +222,14 @@ $continuo = true;
 						$datosCorrectos = true;
 					}else{
 						$datosCorrectos = false;
-						echo "no valido 3";
 					}
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 3";
 					break;
 				}
 			}
 			if($key == "frecep"){
-				echo "valido 4";
 				if(!empty($value)){
 					$fec = strtotime($value);
 					$fecha = date('Y-m-d', $fec);
@@ -249,7 +239,6 @@ $continuo = true;
 						}
 						else{
 							$datosCorrectos = false;
-							echo "no valido 4";
 							break;
 						}
 				}
@@ -258,101 +247,83 @@ $continuo = true;
 				}
 			}
 			if($key == "nombre"){
-				echo "valido 5";
 				if(!empty($value)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 5";
 					break;
 				}
 			}
 			if($key == "titulo"){
-				echo "valido 6";
 				if(!empty($value)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 6";
 					break;
 				}
 			}
 			if($key == "calle"){
-				echo "valido 7";
 				if(!empty($value)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 7";
 					break;
 				}
 			}
 			if($key == "local"){
-				echo "valido 8";
 				if(!empty($value)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 8";
 					break;
 				}
 			}
 			if($key == "prov"){
-				echo "valido 9";
 				if(!empty($value)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 9";
 					break;
 				}
 			}
 			if($key == "numero"){
-				echo "valido 10";
 				if(!empty($value) && filter_var($value, FILTER_VALIDATE_INT)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 10";
 					break;
 				}
 			}
 			if($key == "cp"){
-				echo "valido 11 con valor $value ";
 				if(!empty($value) && filter_var($value, FILTER_VALIDATE_INT)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 11";
 					break;
 				}
 			}
 			if($key == "copias"){
-				echo "valido 12";
 				if(!empty($value) && filter_var($value, FILTER_VALIDATE_INT)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 12";
 					break;
 				}
 			}
 			if($key == "resolucion"){
-				echo "valido 13";
 				if(!empty($value) && filter_var($value, FILTER_VALIDATE_INT)){
 					$datosCorrectos = true;
 				}
 				else{
 					$datosCorrectos = false;
-					echo "no valido 13";
 					break;
 				}
 			}
